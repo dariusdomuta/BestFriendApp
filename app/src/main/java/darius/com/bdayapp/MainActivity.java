b8c929d4451311e96467bd0c90d43fc20f137c03
Domuta Darius <dariusdomuta@gmail.com>
2018-01-15 23:37:58 +0200

    app working fine without UI

--- app/src/main/java/darius/com/bdayapp/MainActivity.java
+++ app/src/main/java/darius/com/bdayapp/MainActivity.java
@@ -2,9 +2,9 @@ package darius.com.bdayapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,20 +12,13 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.ErrorCodes;
-import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.firebase.ui.auth.AuthUI;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,13 +26,11 @@ import org.json.JSONObject;
 
 import java.util.Arrays;
 
-import darius.com.bdayapp.pojos.Datum;
-import darius.com.bdayapp.pojos.UserFeed;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity{
-
-    private static final int RC_SIGN_IN=123;
+    private static final int RC_SIGN_IN = 123;
     private TextView greetingTextView;
+    private Button postLoginButton;
     private Button logoutButton;
     private ProgressBar progressBar;
     private FirebaseAuth auth;
@@ -51,17 +42,20 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         auth = FirebaseAuth.getInstance();
-        context=getApplicationContext();
+        context = getApplicationContext();
 
         progressBar = findViewById(R.id.progress_bar);
         logoutButton = findViewById(R.id.logout_button);
+        postLoginButton = findViewById(R.id.post_login_button);
+
 
         progressBar.setVisibility(View.VISIBLE);
+        postLoginButton.setVisibility(View.GONE);
         logoutButton.setVisibility(View.GONE);
 
         if (auth.getCurrentUser() == null) {
 
-            Log.d("Developer!!! ","User can log in now!!!");
+            Log.d("Developer!!! ", "User can log in now!!!");
 
             AuthUI.IdpConfig facebookIdp = new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER)
                     .setPermissions(Arrays.asList("user_friends", "user_posts"))
@@ -71,15 +65,14 @@ public class MainActivity extends AppCompatActivity{
                     .setAvailableProviders(Arrays.asList(
                             facebookIdp))
                     .build(), RC_SIGN_IN);
-
-
         } else {
 
             Log.d("Developer!!!", "User is already logged in!!!");
 
             progressBar.setVisibility(View.GONE);
             logoutButton.setVisibility(View.VISIBLE);
-    }
+            postLoginButton.setVisibility(View.VISIBLE);
+        }
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,6 +89,14 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+        postLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PostLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
@@ -108,59 +109,9 @@ public class MainActivity extends AppCompatActivity{
 
                 progressBar.setVisibility(View.GONE);
                 logoutButton.setVisibility(View.VISIBLE);
+                postLoginButton.setVisibility(View.VISIBLE);
 
-                new GraphRequest(
-                        AccessToken.getCurrentAccessToken(),
-                        "/me/feed",
-                        null,
-                        HttpMethod.GET,
-                        new GraphRequest.Callback() {
-                            public void onCompleted(GraphResponse response) {
-
-                                JSONObject jsonObject = response.getJSONObject();
-                                try {
-                                    JSONArray jsonArray = jsonObject.getJSONArray("data");
-
-                                    for (int i = 0; i<jsonArray.length(); i++) {
-                                        JSONObject jsonPost = jsonArray.getJSONObject(i);
-                                        if (jsonPost.toString().contains("message")) {
-                                            String message = jsonPost.getString("message");
-                                            String id = jsonPost.getString("id");
-
-                                            if (message.contains("La multi ani") || message.contains("Happy birthday")) {
-                                                Bundle params = new Bundle();
-                                                params.putString("message", "This is a test comment");
-
-                                                new GraphRequest(
-                                                        AccessToken.getCurrentAccessToken(),
-                                                        "/"+id+"/comments",
-                                                        params,
-                                                        HttpMethod.POST,
-                                                        new GraphRequest.Callback() {
-                                                            public void onCompleted(GraphResponse response) {
-                                                                Log.d("Developer!!!", "Comment successful, you bastard!!!");
-                                                            }
-                                                        }
-                                                ).executeAsync();
-                                            }
-                                        }
-
-
-                                    }
-
-
-                                } catch (JSONException e) {
-                                    Log.d ("Developer!!!", "Json exception");
-                                }
-
-                                Log.d("Developer!!!", response.toString());
-
-                            }
-                        }
-                ).executeAsync();
-
-            }
-             else {
+            } else {
                 Log.d("Developer!!!", "Auth error");
             }
         }
--- app/src/main/java/darius/com/bdayapp/PostLoginActivity.java
+++ app/src/main/java/darius/com/bdayapp/PostLoginActivity.java
@@ -1,18 +1,32 @@
 package darius.com.bdayapp;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 /**
  * Created by dariu on 1/14/2018.
@@ -20,33 +34,122 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class PostLoginActivity extends AppCompatActivity {
 
-    private Button logOutButton;
     private FirebaseAuth auth;
-    Context context;
+    private Context context;
+    private HashMap<String, Integer> friends;
+    private int max;
+    private String bestFriendId = "1";
+    private TextView resultTextView;
+
+
+    public int getMax() {
+        return max;
+    }
+
+    public String getBestFriendId() {
+        return bestFriendId;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public void setBestFriendId(String bestFriendId) {
+        this.bestFriendId = bestFriendId;
+    }
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_login_activity);
+
+        resultTextView = findViewById(R.id.result_text_view);
+
         context = getApplicationContext();
         auth = FirebaseAuth.getInstance();
 
-        logOutButton = findViewById(R.id.log_out_button);
-        logOutButton.setText("post login");
-        logOutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AuthUI.getInstance()
-                        .signOut(context)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                Log.d("AUTH", "User Logged Out");
-                                finish();
-                            }
-                        });
-            }
-        });
+        friends = new HashMap<String, Integer>();
 
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me/feed",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+
+                        JSONObject jsonObject = response.getJSONObject();
+                        try {
+                            final JSONArray jsonArray = jsonObject.getJSONArray("data");
+
+                            for ( int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonPost = jsonArray.getJSONObject(i);
+                                String id = jsonPost.getString("id");
+                                new GraphRequest(
+                                        AccessToken.getCurrentAccessToken(),
+                                        "/" + id + "/reactions",
+                                        null,
+                                        HttpMethod.GET,
+                                        new GraphRequest.Callback() {
+                                            public void onCompleted(GraphResponse response) {
+                                                JSONObject post = response.getJSONObject();
+                                                try {
+                                                    JSONArray likes = post.getJSONArray("data");
+
+                                                    for (int j = 0; j < likes.length(); j++) {
+                                                        JSONObject friend = likes.getJSONObject(j);
+                                                        String friendId= friend.getString("id");
+
+                                                        if (friends.containsKey(friendId)) {
+                                                            friends.put(friendId, friends.get(friendId) +1 );
+                                                            if (friends.get(friendId) > getMax()) {
+
+                                                                setMax(friends.get(friendId));
+                                                                setBestFriendId(friendId);
+
+                                                                GraphRequest request = GraphRequest.newGraphPathRequest(
+                                                                        AccessToken.getCurrentAccessToken(),
+                                                                        "/"+getBestFriendId(),
+                                                                        new GraphRequest.Callback() {
+                                                                            @Override
+                                                                            public void onCompleted(GraphResponse response) {
+                                                                                JSONObject name = response.getJSONObject();
+                                                                                try {
+                                                                                    resultTextView.setText(name.getString("name"));
+                                                                                } catch (JSONException e) {
+                                                                                    Log.d("Developer!!!", "JSONException");
+                                                                                }
+
+                                                                            }
+                                                                        });
+
+                                                                request.executeAsync();
+
+
+                                                                Log.d ("Developer!!!", "there you go" + getMax() +" " + getBestFriendId() );
+
+
+                                                            }
+                                                        } else {
+                                                            friends.put(friendId, 1);
+                                                        }
+                                                    }
+
+                                                } catch (JSONException e) {
+                                                    Log.d("Developer!!!", "Json exception");
+                                                }
+                                            }
+                                        }
+                                ).executeAsync();
+                            }
+                        } catch (JSONException e) {
+                            Log.d("Developer!!!", "Json exception");
+                        }
+                    }
+
+                }
+        ).executeAsync();
     }
+
 }
